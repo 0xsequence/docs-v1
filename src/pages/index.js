58e4c41ae8eafd147fa0651fa40a8ca6760d5cd4
Cat McGee <helloworld@mcgee.cat>
2022-09-30 22:11:54 +0100

    structure, colour, lowercase web3

--- src/pages/index.js
+++ src/pages/index.js
@@ -3,7 +3,6 @@ import Layout from '@theme/Layout'
 import Link from '@docusaurus/Link'
 import useDocusaurusContext from '@docusaurus/useDocusaurusContext'
 
-import './styles.module.css'
 import styled from '@emotion/styled'
 
 import useBaseUrl from '@docusaurus/useBaseUrl'
@@ -20,33 +19,33 @@ const actions = [
   {
     title: 'Multichain support',
     href: '#',
-    to: '/',
+    to: '/multi-chain-support',
     text: `Find out which chains are supported by the developer platform`,
   },
   {
     title: 'Gas fees',
     href: '#',
-    to: '/',
-    text: `Learn how to minimize, bundle, and remove gas fees for your users with Sequence`,
+    to: '/intro/gas',
+    text: `Learn how to minimize, bundle, and remove gas fees for your users`,
   },
 ]
 
 const github = [
   {
     title: 'Wallet',
-    href: '#',
+    href: '/wallet',
   },
   {
     title: 'Indexer',
-    href: '#',
+    href: '/indexer',
   },
   {
     title: 'Metadata',
-    href: '#',
+    href: 'metadata',
   },
   {
     title: 'Relayer',
-    href: '#',
+    href: '/relayer',
   },
 ]
 
@@ -54,22 +53,22 @@ const Guides = [
   {
     title: 'Quickstart',
     text: 'Get started building using the JavaScript SDK',
-    to: '/',
+    to: '/quickstart',
   },
   {
     title: 'Connect wallet',
     text: 'Integrate the smart contract wallet into your app',
-    to: '/',
+    to: '/wallet/guides/connect-wallet',
   },
   {
     title: 'Verifying messages',
     text: 'Signing transactions',
-    to: '/',
+    to: '/wallet/guides/sign-message',
   },
   {
     title: 'Send an ERC-20 token',
     text: 'Learn about transfers on Sequence',
-    to: '/',
+    to: '/wallet/guides/send-erc20',
   },
 ]
 
@@ -158,7 +157,6 @@ const ShadowCard = styled(Card)`
   background-color: #ffffff10;
   backdrop-filter: blur(10px);
   min-height: 200px;
-  /* background-color: var(--ifm-color-emphasis-0); */
 `
 
 const WideCard = styled(ShadowCard)`
@@ -188,7 +186,7 @@ const LinkRow = styled.div`
   display: flex;
   flex-direction: row;
   a h3 {
-    color: black !important;
+    color: black;
   }
 `
 
@@ -200,12 +198,6 @@ const DocsHeader = styled.div`
   overflow: hidden;
   width: 100%;
   position: relative;
-`
-
-const StyledGithubIcon = styled.div`
-  svg {
-    fill: var(--ifm-font-color-base);
-  }
 `
 
 const HideMedium = styled.div`
@@ -241,9 +233,9 @@ export default function Home() {
                   <TopSection>
 
                     <svg
-                      style={{ width: '24px', opacity: 0.2 }}
+                      style={{ width: '24px', opacity:0.9 }}
                       xmlns="http://www.w3.org/2000/svg"
-                      fill="currentColor"
+                      fill="white"
                       viewBox="0 0 24 24"
                     >
                       <path d="M20 4h1a1 1 0 00-1-1v1zm-1 12a1 1 0 102 0h-2zM8 3a1 1 0 000 2V3zM3.293 19.293a1 1 0 101.414 1.414l-1.414-1.414zM19 4v12h2V4h-2zm1-1H8v2h12V3zm-.707.293l-16 16 1.414 1.414 16-16-1.414-1.414z" />
@@ -297,50 +289,11 @@ export default function Home() {
             {github.map((action) => (
               <Link style={{ textDecoration: 'none' }} href={action.href}>
                 <Card key={action.title} style={{ marginBottom: '1rem' }}>
-                  <LinkRow>
-                    <StyledGithubIcon style={{ display: 'flex', alignItems: 'center' }}>
-                      <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 120.78 117.79" style={{ width: '24px' }}>
-                        <defs></defs>
-                        <title>testlogo</title>
-                        <g id="Layer_2" data-name="Layer 2">
-                          <g id="Layer_1-2" data-name="Layer 1">
-                            <path
-                              className="cls-1"
-                              d="M60.39,0A60.39,60.39,0,0,0,41.3,117.69c3,.56,4.12-1.31,4.12-2.91,0-1.44-.05-6.19-.08-11.24C28.54,107.19,25,96.42,25,96.42c-2.75-7-6.71-8.84-6.71-8.84-5.48-3.75.41-3.67.41-3.67,6.07.43,9.26,6.22,9.26,6.22,5.39,9.23,14.13,6.57,17.57,5,.55-3.9,2.11-6.56,3.84-8.07C36,85.55,21.85,80.37,21.85,57.23A23.35,23.35,0,0,1,28.08,41c-.63-1.52-2.7-7.66.58-16,0,0,5.07-1.62,16.61,6.19a57.36,57.36,0,0,1,30.25,0C87,23.42,92.11,25,92.11,25c3.28,8.32,1.22,14.46.59,16a23.34,23.34,0,0,1,6.21,16.21c0,23.2-14.12,28.3-27.57,29.8,2.16,1.87,4.09,5.55,4.09,11.18,0,8.08-.06,14.59-.06,16.57,0,1.61,1.08,3.49,4.14,2.9A60.39,60.39,0,0,0,60.39,0Z"
-                            />
-                            <path
-                              className="cls-2"
-                              d="M22.87,86.7c-.13.3-.6.39-1,.19s-.69-.61-.55-.91.61-.39,1-.19.69.61.54.91Z"
-                            />
-                            <path
-                              className="cls-2"
-                              d="M25.32,89.43c-.29.27-.85.14-1.24-.28a.92.92,0,0,1-.17-1.25c.3-.27.84-.14,1.24.28s.47,1,.17,1.25Z"
-                            />
-                            <path
-                              className="cls-2"
-                              d="M27.7,92.91c-.37.26-1,0-1.35-.52s-.37-1.18,0-1.44,1,0,1.35.51.37,1.19,0,1.45Z"
-                            />
-                            <path
-                              className="cls-2"
-                              d="M31,96.27A1.13,1.13,0,0,1,29.41,96c-.53-.49-.68-1.18-.34-1.54s1-.27,1.56.23.68,1.18.33,1.54Z"
-                            />
-                            <path
-                              className="cls-2"
-                              d="M35.46,98.22c-.15.47-.82.69-1.51.49s-1.13-.76-1-1.24.82-.7,1.51-.49,1.13.76,1,1.24Z"
-                            />
-                            <path
-                              className="cls-2"
-                              d="M40.4,98.58c0,.5-.56.91-1.28.92s-1.3-.38-1.31-.88.56-.91,1.29-.92,1.3.39,1.3.88Z"
-                            />
-                            <path
-                              className="cls-2"
-                              d="M45,97.8c.09.49-.41,1-1.12,1.12s-1.35-.17-1.44-.66.42-1,1.12-1.12,1.35.17,1.44.66Z"
-                            />
-                          </g>
-                        </g>
-                      </svg>
+                  <LinkRow style={{ display: 'flex', alignItems: 'center' }}>
+                   
+                    
                       <h3 style={{ marginBottom: '0rem', marginLeft: '16px' }}>{action.title}</h3>
-                    </StyledGithubIcon>
+                   
                     <svg
                       style={{ width: '24px', height: '24px', opacity: 0.2 }}
                       xmlns="http://www.w3.org/2000/svg"
@@ -385,7 +338,6 @@ export default function Home() {
           </WideCard>
         </Link>
       </Container>
-      hi
     </Layout>
   )
 }
