2afd0a7eebc9cebd3506cb67d676ac65950d89ef
Taylan Pince <taylanpince@gmail.com>
2024-03-07 10:08:53 +0100

    Fix positions for categories

--- sidebars.js
+++ sidebars.js
@@ -18,7 +18,7 @@ module.exports = {
   //   {
   //     type: 'category',
   //     label: 'Introduction',
-  //     items: ['wallet/wallet-contracts/why'],
+  //     items: ['intro/overview'],
   //   },
   // ],
    
